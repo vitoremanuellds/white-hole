6c7a19ebf6a49c82e0ff97a07963ca52689770ee
Vitor Emanuel <vemanuel.wt@gmail.com>
2022-07-14 15:53:28 -0300

    Adding some more functionalities.

--- white-hole/src/Db/Operations.hs
+++ white-hole/src/Db/Operations.hs
@@ -6,13 +6,15 @@ import Db.Configure
 import Entities.User
 import qualified Entities.Movie as M
 import qualified Entities.Rating as R
+import qualified Entities.Serie as S
 import Data.List
-import Data.Time
+import Data.Char (toLower)
+
 
 createUser :: Connection -> User -> IO Bool
 createUser conn user = do
     exists <- userAlreadyExists conn $ email user
-    if not exists then do 
+    if not exists then do
         ok <- execute conn "INSERT INTO users (email, passwd, nome, sobrenome) values (?,?,?,?);" (email user, password user, nome user, sobrenome user)
         return True
     else return False
@@ -22,7 +24,7 @@ getUserByEmail :: Connection -> String -> IO [User]
 getUserByEmail conn userEmail = do
     let q = "SELECT * FROM users WHERE email = ?;"
     query conn q [userEmail] :: IO [User]
-    
+
 
 userAlreadyExists :: Connection -> String -> IO Bool
 userAlreadyExists conn userEmail = do
@@ -54,7 +56,7 @@ avaluateMovie conn user movie rating commentary = do
         return True
     else do
         return False
-        
+
 
 getRatings :: Connection -> M.Movie -> IO [R.Rating]
 getRatings conn movie = do
@@ -66,14 +68,14 @@ getCasting conn movie = undefined
 
 
 
-titleContainsWord :: [String] -> M.Movie -> Bool
-titleContainsWord [] _ = False
-titleContainsWord (x:xs) movie = x `isInfixOf` M.title movie || titleContainsWord xs movie
+titleContainsWordMovie :: [String] -> M.Movie -> Bool
+titleContainsWordMovie [] _ = False
+titleContainsWordMovie (x:xs) movie = x `isInfixOf` map toLower (M.title movie) || titleContainsWordMovie xs movie
 
 
 filterMovies :: [M.Movie] -> [String] -> [M.Movie]
 filterMovies [] _ = []
-filterMovies (x:xs) names = if titleContainsWord names x then x:filterMovies xs names else filterMovies xs names
+filterMovies (x:xs) names = if titleContainsWordMovie names x then x:filterMovies xs names else filterMovies xs names
 
 
 searchMovie :: Connection -> String -> IO [M.Movie]
@@ -82,13 +84,79 @@ searchMovie conn title = do
     movies <- query_ conn "SELECT * FROM movies m;" :: IO [M.Movie]
     return (filterMovies movies qWords)
 
+titleContainsWordSerie :: [String] -> S.Serie -> Bool
+titleContainsWordSerie [] _ = False
+titleContainsWordSerie (x:xs) serie = x `isInfixOf` map toLower (S.title serie) || titleContainsWordSerie xs serie
 
-getPlataformToWatch :: Connection -> M.Movie -> IO [String]
-getPlataformToWatch conn movie = undefined
+filterSeries :: [S.Serie] -> [String] -> [S.Serie]
+filterSeries [] _ = []
+filterSeries (x:xs) names = if titleContainsWordSerie names x then x:filterSeries xs names else filterSeries xs names
+
+searchSerie :: Connection -> String -> IO [S.Serie]
+searchSerie conn title = do
+    let qWords = words title
+    series <- query_ conn "SELECT * FROM series s;" :: IO [S.Serie]
+    return (filterSeries series qWords)
+
+
+getPlataformToWatchMovies :: Connection -> M.Movie -> IO [String]
+getPlataformToWatchMovies conn movie = undefined
+
+getPlataformToWatchSeries :: Connection -> S.Serie -> IO [String]
+getPlataformToWatchSeries conn serie = undefined
 
 
 getWatchLaterList :: Connection -> User -> IO [M.Movie]
 getWatchLaterList conn user = do
-    movies <- query conn "SELECT * FROM watchlaterlist w WHERE w.useremail = ?" [email user] :: IO [M.Movie]
-    return movies
+    query conn "SELECT * FROM watchlaterlist w WHERE w.useremail = ?" [email user] :: IO [M.Movie]
+
+
+registerMovie :: Connection -> String -> String -> String -> String -> IO M.Movie
+registerMovie conn title releaseDate summary duration = do
+    execute conn "INSERT INTO movies(title, releasedate, duration, summary) VALUES (?,?,?,?)" (title, releaseDate, summary, duration)
+    movie <- query conn "SELECT * FROM movies WHERE title=?;" [title] :: IO [M.Movie]
+    return (head movie)
+
+
+registerSerie :: Connection -> String -> String -> String -> String -> IO S.Serie
+registerSerie conn title releaseDate episodes summary = do
+    execute conn "INSERT INTO series(title, releasedate, episodes, summary) VALUES (?,?,?,?);" (title, releaseDate, episodes, summary)
+    serie <- query conn "SELECT * FROM series WHERE title=?;" [title] :: IO [S.Serie]
+    return (head serie)
+
+
+addCastingToMovie :: Connection -> M.Movie -> [String] -> IO Bool
+addCastingToMovie conn movie [] = return True
+addCastingToMovie conn movie actors = do
+    execute conn "INSERT INTO casting VALUES (?,?, 'actor')" (M.movieId movie, head actors)
+    addCastingToMovie conn movie (tail actors)
+
+
+addCastingToSerie :: Connection -> S.Serie -> [String] -> IO Bool
+addCastingToSerie conn serie [] = return True
+addCastingToSerie conn serie actors = do
+    execute conn "INSERT INTO seriescasting VALUES (?,?, 'actor')" (S.serieId serie, head actors)
+    addCastingToSerie conn serie (tail actors)
+
+
+addDirectorsToMovie :: Connection -> M.Movie -> [String] -> IO Bool
+addDirectorsToMovie conn movie [] = return True
+addDirectorsToMovie conn movie directors = do
+    execute conn "INSERT INTO casting VALUES (?,?, 'director')" (M.movieId movie, head directors)
+    addDirectorsToMovie conn movie (tail directors)
+
+
+addDirectorsToSerie :: Connection -> S.Serie -> [String] -> IO Bool
+addDirectorsToSerie conn serie [] = return True
+addDirectorsToSerie conn serie directors = do
+    execute conn "INSERT INTO seriescasting VALUES (?,?, 'director')" (S.serieId serie, head directors)
+    addDirectorsToSerie conn serie (tail directors)
+
+
+updateMovieRating :: Connection -> M.Movie -> IO Float
+updateMovieRating conn movie = do
+    [Only rating] <- query conn "SELECT AVG(ratingId) FROM movies WHERE movieid=?;" [M.movieId movie] :: IO [Only Float]
+    execute conn "UPDATE movies SET rating = ? WHERE movieid = ?;" (rating, M.movieId movie)
+    return rating
+
 
--- white-hole/src/Main.hs
+++ white-hole/src/Main.hs
@@ -13,83 +13,83 @@ import Data.Char (toLower, toUpper, isNumber, isAlphaNum, isDigit)
 
 signUp :: Connection -> IO()
 signUp conn = do
-  putStrLn "Digite o seu nome:"
-  userName <- getLine
-  putStrLn "Digite o seu sobrenome:"
-  userSurname <- getLine
-  putStrLn "Digite o seu e-mail:"
-  userEmail <- getLine
-  putStrLn "Digite a sua senha:"
-  userPassword <- getLine
-  {- Chamada da função que cadastra o usuário no banco de dados! -}
-  ok <- createUser conn $ User {email=userEmail, password=userPassword, nome=userName, sobrenome=userSurname}
-  putStrLn (if ok then "Usuario cadastrado com sucesso!" else "Ja existe usuario cadastrado com esse e-mail!")
+    putStrLn "Digite o seu nome:"
+    userName <- getLine
+    putStrLn "Digite o seu sobrenome:"
+    userSurname <- getLine
+    putStrLn "Digite o seu e-mail:"
+    userEmail <- getLine
+    putStrLn "Digite a sua senha:"
+    userPassword <- getLine
+    {- Chamada da função que cadastra o usuário no banco de dados! -}
+    ok <- createUser conn $ User {email=userEmail, password=userPassword, nome=userName, sobrenome=userSurname}
+    putStrLn (if ok then "Usuario cadastrado com sucesso!" else "Ja existe usuario cadastrado com esse e-mail!")
 
 
 signIn :: Connection -> IO()
 signIn conn = do
-  putStrLn "Digite o seu e-mail:"
-  userEmail <- getLine
-  putStrLn "Digite a sua senha:"
-  userPassword <- getLine
-  {- Chamada da função run que roda as opções de avaliação e de 
-  pesquisa, assim como os rankings! -}
-  authenticated <- authenticate conn userEmail userPassword
-  if authenticated then do
-    putStrLn "Bem vindo!"
-    user <- getUserByEmail conn userEmail
-    run conn $ head user
-  else do
-    putStrLn "E-mail ou senha inválidos!"
-    firstMenu conn
+    putStrLn "Digite o seu e-mail:"
+    userEmail <- getLine
+    putStrLn "Digite a sua senha:"
+    userPassword <- getLine
+    {- Chamada da função run que roda as opções de avaliação e de 
+    pesquisa, assim como os rankings! -}
+    authenticated <- authenticate conn userEmail userPassword
+    if authenticated then do
+        putStrLn "Bem vindo!"
+        user <- getUserByEmail conn userEmail
+        run conn $ head user
+    else do
+        putStrLn "E-mail ou senha inválidos!"
+        firstMenu conn
 
 
 firstMenu :: Connection -> IO()
 firstMenu conn = do
-  putStrLn "Bem vindo ao White Hole de filmes e séries!"
-  putStrLn "Descubra novos filmes para assistir e avalie os que você já assistiu!"
-  putStrLn ""
-  putStrLn "1 - Logar no White Hole"
-  putStrLn "2 - Cadastrar-se no White Hole"
-  putStrLn "3 - Sair"
-  putStrLn ""
-  putStrLn "Digite a opção que deseja acessar:"
-  option <- getLine
-  if option == "1" then do
-    signIn conn
-  else if option == "2" then do
-    signUp conn >> firstMenu conn
-  else if option == "3" then do
-    putStrLn "Obrigado, tenha um ótimo dia!"
-    exitSuccess
-  else do
-    putStrLn "Digite uma opção válida na próxima!"
+    putStrLn "Bem vindo ao White Hole de filmes e séries!"
+    putStrLn "Descubra novos filmes para assistir e avalie os que você já assistiu!"
     putStrLn ""
+    putStrLn "1 - Logar no White Hole"
+    putStrLn "2 - Cadastrar-se no White Hole"
+    putStrLn "3 - Sair"
+    putStrLn ""
+    putStrLn "Digite a opção que deseja acessar:"
+    option <- getLine
+    if option == "1" then do
+        signIn conn
+    else if option == "2" then do
+        signUp conn >> firstMenu conn
+    else if option == "3" then do
+        putStrLn "Obrigado, tenha um ótimo dia!"
+        exitSuccess
+    else do
+        putStrLn "Digite uma opção válida na próxima!"
+        putStrLn ""
 
 
 run :: Connection -> User -> IO()
 run conn user = do
-  putStrLn ""
-  putStrLn "1 - Procurar por filme"
-  putStrLn "2 - Minha lista de marcados para assistir depois"
-  putStrLn "3 - 10 melhores filmes"
-  putStrLn "4 - 10 melhores séries"
-  putStrLn "5 - 10 melhores filmes por categoria"
-  putStrLn "6 - 10 melhores séries por categoria"
-  putStrLn "7 - Logout"
-  putStrLn ""
-  putStrLn "Digite a opção desejada: "
-  hFlush stdout
-  option <- getLine
-  case option of
-    "1" -> search conn user
-    "2" -> myList conn user
-    "3" -> tenBestMovies conn
-    "4" -> tenBestSeries conn
-    "5" -> tenBestMoviesByCategory conn
-    "6" -> tenBestSeriesByCategory conn
-    "7" -> firstMenu conn
-    x -> putStrLn "Digite uma opção válida" >> run conn user
+    putStrLn ""
+    putStrLn "1 - Procurar por filme"
+    putStrLn "2 - Minha lista de marcados para assistir depois"
+    putStrLn "3 - 10 melhores filmes"
+    putStrLn "4 - 10 melhores séries"
+    putStrLn "5 - 10 melhores filmes por categoria"
+    putStrLn "6 - 10 melhores séries por categoria"
+    putStrLn "7 - Logout"
+    putStrLn ""
+    putStrLn "Digite a opção desejada: "
+    hFlush stdout
+    option <- getLine
+    case option of
+        "1" -> search conn user
+        "2" -> myList conn user
+        "3" -> tenBestMovies conn
+        "4" -> tenBestSeries conn
+        "5" -> tenBestMoviesByCategory conn
+        "6" -> tenBestSeriesByCategory conn
+        "7" -> firstMenu conn
+        x -> putStrLn "Digite uma opção válida" >> run conn user
 
 isANumber :: String -> Bool -> Bool
 isANumber [] _ = True
@@ -98,74 +98,151 @@ isANumber (x:xs) True = isDigit x && isANumber xs True
 
 search :: Connection -> User -> IO()
 search conn user = do
-  putStrLn "----------PESQUISAR----------"
-  putStrLn ""
-  putStrLn "Digite o nome do filme que deseja pesquisar"
-  putStrLn ""
-  movie <- getLine
-  putStrLn ""
-  movies <- searchMovie conn (map toLower movie)
-  printMoviesList movies 1
-  putStrLn ""
+    putStrLn "---------------------- PESQUISAR ------------------------"
+    putStrLn ""
+    putStrLn "Digite o nome do filme ou série que deseja pesquisar"
+    putStrLn ""
+    movie <- getLine
+    putStrLn ""
+    movies <- searchMovie conn (map toLower movie)
+    if null movies then putStrLn "Não foi encontrado nenhum filme ou série com esse título!" else printMoviesList movies 1
+    putStrLn ""
 
-  putStrLn "Digite o número correspondente ao filme que quer ver (0 - Pesquisar novamente, -1 - Voltar ao menu principal): "
-  putStrLn ""
+    putStrLn "Digite o número correspondente ao filme que quer ver "
+    putStrLn "(0 - Pesquisar novamente, -1 - Voltar ao menu principal, -2 - Cadastrar um novo filme ou série): "
+    putStrLn ""
 
-  option <- getLine
-  if option == "-1" then
-    run conn user
-  else if option == "0" then
-    search conn user
-  else if isANumber option True || (read option :: Int) > length movies then do
-    putStrLn "Digite uma opção válida na próxima vez."
-  else do
-    -- showMovie option 
-    putStrLn "-- showMovie option"
+    option <- getLine
+    if option == "-1" then
+        run conn user
+    else if option == "0" then
+        search conn user
+    else if option == "-2" then
+        registerMovieSerie conn user
+    else if isANumber option True || (read option :: Int) > length movies then do
+        putStrLn "Digite uma opção válida na próxima vez."
+    else do
+        -- showMovie option 
+        putStrLn "-- showMovie option"
 
 
-{-Funções criadas apenas para testar a função run-}
+registerMovieSerie :: Connection -> User -> IO ()
+registerMovieSerie conn user = do
+    putStrLn ""
+    putStrLn "1 - Adicionar filme"
+    putStrLn "2 - Adicionar série"
+    putStrLn ""
+    putStrLn "Digite a opção:"
+    option <- getLine
+    case option of
+        "1" -> createMovie conn >> search conn user
+        "2" -> createSerie conn >> search conn user
+        x -> putStrLn "Digite uma opção válida!" >> registerMovieSerie conn user
+
+createMovie :: Connection -> IO ()
+createMovie conn = do
+    putStrLn ""
+    putStrLn "Qual o título do filme?"
+    title <- getLine
+    if null title then 
+        putStrLn "O filme deve ter um título!" >> createMovie conn 
+    else
+        putStrLn "Qual a data de lançamento do filme? (yyyy-mm-dd)"
+
+    releaseDate <- getLine
+    putStrLn "Qual a duração do filme? (Em minutos)"
+    duration <- getLine
+    putStrLn "Qual a sinopse do filme?"
+    summaryT <- getLine
+    putStrLn "Digite o nome dos diretores (separados por vírgula):"
+    directors <- getLine
+    putStrLn "Digite o nome dos atores (separados por vírgula):"
+    actors <- getLine
+    let summary = if summaryT == "" then "No comments!" else summaryT
+    movie <- registerMovie conn title releaseDate duration summary
+    addCastingToMovie conn movie (splitItems actors [])
+    addDirectorsToMovie conn movie (splitItems directors [])
+    putStrLn ""
+    putStrLn "Filme cadastrado com sucesso!"
+
+
+
+createSerie :: Connection -> IO ()
+createSerie conn = do
+    putStrLn ""
+    putStrLn "Qual o título da série?"
+    title <- getLine
+    if null title then 
+        putStrLn "O filme deve ter um título!" >> createMovie conn 
+    else 
+        putStrLn "Qual a data de lançamento da série? (yyyy-mm-dd)"
+    releaseDate <- getLine
+    putStrLn "Qual a quantidade de episódios da série?"
+    duration <- getLine
+    putStrLn "Qual a sinopse da série?"
+    summaryT <- getLine
+    putStrLn "Digite o nome dos diretores (separados por vírgula):"
+    directors <- getLine
+    putStrLn "Digite o nome dos atores (separados por vírgula):"
+    actors <- getLine
+    let summary = if summaryT == "" then "No comments!" else summaryT
+    serie <- registerSerie conn title releaseDate duration summary
+    addCastingToSerie conn serie (splitItems actors [])
+    addDirectorsToSerie conn serie (splitItems directors [])
+    putStrLn ""
+    putStrLn "Série cadastrado com sucesso!"
+
+splitItems :: [Char] -> [String] -> [String]
+splitItems [] result = reverse result
+splitItems names result = do
+    let x = takeWhile (/= ',') names
+    let y = dropWhile (/= ',') names
+    let z = if null y then y else tail y
+    splitItems z (x:result)
+
+
+    {-Funções criadas apenas para testar a função run-}
 myList :: Connection -> User -> IO ()
 myList conn user = do
-  putStrLn "------ Minha Lista Para Assistir Depois ------"
-  putStrLn ""
-  movies <- getWatchLaterList conn user
-  if null movies then do
-    print "Lista vazia"
-    run conn user
-  else do
-    printMoviesList movies 1
-
-  putStrLn ""
-  putStrLn "Opções:"
-  putStrLn ""
-  putStrLn "1 - Acessar filme"
-  putStrLn "2 - Voltar"
-  putStrLn ""
-  putStrLn "Digite sua ação"
-  option <- getLine
-  if option == "1" then do
+    putStrLn "------ Minha Lista Para Assistir Depois ------"
     putStrLn ""
-    putStrLn "Qual filme você quer acessar? (Digite o número do filme da lista)"
-    filme <- getLine
-    if (read filme :: Int) > length movies || (read filme :: Int) <= 0 then do
-      putStrLn "digite uma opção válida"
-      myList conn user
+    movies <- getWatchLaterList conn user
+    if null movies then do
+        print "Lista vazia"
+        run conn user
     else do
-      showMovie conn (movies !! (read filme :: Int))
-  else do
-    if option == "2" then do
-      run conn user
+        printMoviesList movies 1
+    putStrLn ""
+    putStrLn "Opções:"
+    putStrLn ""
+    putStrLn "1 - Acessar filme"
+    putStrLn "2 - Voltar"
+    putStrLn ""
+    putStrLn "Digite sua ação"
+    option <- getLine
+    if option == "1" then do
+        putStrLn ""
+        putStrLn "Qual filme você quer acessar? (Digite o número do filme da lista)"
+        filme <- getLine
+        if (read filme :: Int) > length movies || (read filme :: Int) <= 0 then do
+            putStrLn "digite uma opção válida"
+            myList conn user
+        else do
+            showMovie conn (movies !! (read filme :: Int))
     else do
-      putStrLn "Digite uma opção válida"
-      myList conn user
+        if option == "2" then do
+            run conn user
+        else do
+            putStrLn "Digite uma opção válida"
+            myList conn user
 
 
 
 printMoviesList :: [M.Movie] -> Integer -> IO ()
 printMoviesList [] _ = return ()
 printMoviesList movies seqNum = do
-  putStrLn (show seqNum ++ " - " ++ capitalize (M.title (head movies)))
-  printMoviesList (tail movies) (seqNum + 1)
+    putStrLn (show seqNum ++ " - " ++ capitalize (M.title (head movies)))
+    printMoviesList (tail movies) (seqNum + 1)
 
 
 capitalize :: String -> String
@@ -196,8 +273,8 @@ showSerie conn serie = undefined
 
 main :: IO ()
 main = do
-  conn <- connectToDB
-  firstMenu conn
+    conn <- connectToDB
+    firstMenu conn
 
 
 
